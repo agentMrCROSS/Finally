f41e32486cb9e129bb7578f6d659af2c2917c1b1
Paul <baron-andor@mail.ru>
2023-08-16 14:49:32 +0500

    Реализовал модель + зарегистрировал ее в админ.ру

--- firstsprint/pereval/admin.py
+++ firstsprint/pereval/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Pass
 
-# Register your models here.
+admin.site.register(Pass)
--- firstsprint/pereval/models.py
+++ firstsprint/pereval/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Pass(models.Model):
+    beauty_title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    other_titles = models.CharField(max_length=255)
+    connect = models.TextField(blank=True)
+    add_time = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    coords = models.ForeignKey(Coordinate, on_delete=models.CASCADE)
+    level = models.JSONField(default=dict, blank=True)
+    images = models.ManyToManyField(Image)
+    status = models.CharField(max_length=255, choices=(
+        ('new', 'New'),
+        ('pending', 'Pending'),
+        ('accepted', 'Accepted'),
+        ('rejected', 'Rejected')
+    ))
+
+    def create_pass(self, pass_data):
+        # Создание новой записи перевала
+        pass_object = self.create(**pass_data)
+        # Установка значения поля status в "new"
+        pass_object.status = "new"
+        pass_object.save()
+        return pass_object
