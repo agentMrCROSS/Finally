c43424f53c313e08eb5bcffcba7b433e4729b190
Paul <baron-andor@mail.ru>
2023-08-19 17:31:28 +0500

    новый сериализер

--- firstsprint/pereval/serializers.py
+++ firstsprint/pereval/serializers.py
@@ -1,73 +1,31 @@
 from .models import *
 from rest_framework import serializers
-from drf_writable_nested import WritableNestedModelSerializer
-
-
-class CoordsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Coords
-        fields = '__all__'
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Users
-        fields = '__all__'
+        model = User
+        fields = ['email', 'fam', 'name', 'otc', 'phone']
 
 
-class PhotoSerializer(serializers.ModelSerializer):
-    # data = serializers.URLField()
-
+class CoordinateSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Photo
-        # fields = '__all__'
+        model = Coordinate
+        fields = ['latitude', 'longitude', 'height']
+
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
         fields = ['data', 'title']
 
 
-class Pereval_addedSerializer(WritableNestedModelSerializer):
-    photos = PhotoSerializer(many=True)
-    user = UserSerializer()
-    coords = CoordsSerializer()
-
-    class Meta:
-        model = Pereval_added
-        fields = '__all__'
-        # exclude = ['status']
-
-    def create(self, validated_data):
-        photos_data = validated_data.pop('photos', [])
-        user_data = validated_data.pop('user')
-        coords_data = validated_data.pop('coords')
-
-        user_serializer = UserSerializer(data=user_data)
-        user_serializer.is_valid(raise_exception=True)
-        user = user_serializer.save()
-
-        coords_serializer = CoordsSerializer(data=coords_data)
-        coords_serializer.is_valid(raise_exception=True)
-        coords = coords_serializer.save()
-
-        pereval = Pereval_added.objects.create(user=user, coords=coords, **validated_data)
-
-        # for photo_data in photos_data:
-        #     photo_serializer = PhotoSerializer(data=photo_data)
-        #     photo_serializer.is_valid(raise_exception=True)
-        #     photo = photo_serializer.save(pereval=pereval)
-
-        for photo_data in photos_data:
-            data = photo_data.pop('data')
-            title = photo_data.pop('title')
-            Photo.objects.create(data=data, title=title, pereval=pereval)
-
-        return pereval
-
-
-# для проверки
-class PerevalSerializer(WritableNestedModelSerializer, serializers.ModelSerializer):
-    coords = CoordsSerializer()
-    # photo = PhotoSerializer()
+class PassSerializer(serializers.ModelSerializer):
+    coords = CoordinateSerializer()
+    images = ImageSerializer(many=True)
     user = UserSerializer()
 
     class Meta:
-        model = Pereval_added
-        exclude = ['add_time']
+        model = Pass
+        fields = ['beauty_title', 'title', 'other_titles', 'connect', 'add_time', 'user',
+                  'coords', 'level', 'images', 'status']
