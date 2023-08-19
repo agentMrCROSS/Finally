7c4609b88cd5b4d9f674319f0fcf7db8380abe09
Paul <baron-andor@mail.ru>
2023-08-19 17:24:01 +0500

    другие урлс более понятные

--- firstsprint/firstsprint/urls.py
+++ firstsprint/firstsprint/urls.py
@@ -16,16 +16,28 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from pereval.views import PerevalViewSet, Pereval_addedAPICreate
 from rest_framework import routers
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+from django.views.generic import TemplateView
 
-router = routers.SimpleRouter()
-router.register(r'pereval', PerevalViewSet)
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Mountains API",
+        default_version="v1",
+        description="This API provides access to data and services for our project",
+        terms_of_service="",
+        contact=openapi.Contact(email="contact@example.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pereval.urls')),
-    # для проверки
-    path('api/v1/', include(router.urls))
+    path('swagger-ui/', TemplateView.as_view(template_name='swagger-ui.html'), name='swagger-ui'),
+    path('api/', include('pereval.urls')),
 ]
